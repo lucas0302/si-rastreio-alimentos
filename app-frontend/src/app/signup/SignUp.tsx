983781fce2e8aa3fd76d8c3ef13c988ce5a357bc
lucas araujo <lucasao744@gmail.com>
2025-09-03 23:37:09 -0300

    ajustes

---
+++ app-frontend/src/app/signup/SignUp.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function SignUp() {
+  return (
+    <>
+      <h1>Signup Page</h1>
+    </>
+  );
+}
---
+++ app-frontend/src/constants/colors.ts
@@ -0,0 +1,5 @@
+export const Colors = {
+  primary: '#1E90FF',
+  secondary: '#FF69B4',
+  background: '#F5F5F5',
+};
