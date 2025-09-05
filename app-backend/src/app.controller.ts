66d7dca09cde66c991bbafdf3bbb58b4128bac09
lucas araujo <lucasao744@gmail.com>
2025-09-05 15:17:16 -0300

    ajustes

--- app-backend/src/app.controller.ts
+++ app-backend/src/app.controller.ts
@@ -3,10 +3,11 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
+    console.log('Hello World!');
     return this.appService.getHello();
   }
 }
