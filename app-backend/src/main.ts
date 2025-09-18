ab9805eb9879a9afcfe7cf725f3f1b2329dea4e5
lucas araujo <lucasao744@gmail.com>
2025-09-18 17:31:14 -0300

    backend-structure

--- app-backend/src/main.ts
+++ app-backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitando CORS
+  app.enableCors({
+    origin: ['http://localhost:3000', process.env.FRONTEND_URL], // substitua pelas origens permitidas
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // se precisar enviar cookies ou Authorization headers
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove propriedades não declaradas nos DTOs
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- app-backend/src/users/users.service.ts
+++ app-backend/src/users/users.service.ts
@@ -4,28 +4,6 @@ import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class UsersService {
-  constructor(
-    private prisma: PrismaService,
-    // private readonly hasingService: HashingServiceProtocol
-  ) { }
-
-
-
-
-
-  async create(createUserDto: CreateUserDto) {
-
-    try {
-
-
-      return "deu bom";
-
-    } catch (err) {
-      console.log(err);
-      throw new HttpException('Erro ao cadastrar usuario.', HttpStatus.BAD_REQUEST);
-    }
-
-  }
 
 
 }
