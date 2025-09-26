c76b02eb22893c5cc4999cfb217341f6554492ac
RhuanLino <rhuanlino309@gmail.com>
2025-09-26 00:31:28 -0300

    @RhuanLino feature | criando endpoint cadastro produtos

--- app-backend/src/products/dto/create-product.dto.ts
+++ app-backend/src/products/dto/create-product.dto.ts
@@ -1,1 +1,33 @@
-export class CreateProductDto {}
+import { IsNotEmpty, IsString, MinLength, MaxLength, Matches, IsEnum, IsInt, IsDecimal, isNotEmpty } from 'class-validator';
+
+export class CreateProductDto {
+    @IsNotEmpty()
+    @IsInt()
+    code: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(3)
+    @MaxLength(255)
+    name: string;
+
+    @IsNotEmpty()
+    @IsDecimal()
+    price: number;
+
+    @IsNotEmpty()
+    @IsDecimal()
+    weight: number;
+
+    @IsNotEmpty()
+    @IsString()
+    unit: string;
+
+    @IsNotEmpty()
+    @IsDecimal()
+    expiration: string;
+
+    @IsNotEmpty()
+    @IsString()
+    expiration_unit: string;
+}
