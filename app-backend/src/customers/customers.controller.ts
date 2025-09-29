96acce3ef90e6158d934e61ae7d541289e356953
lucas araujo <lucasao744@gmail.com>
2025-09-28 22:01:22 -0300

    add rotas autentificadas no clientes bacl-end

--- app-backend/src/customers/customers.controller.ts
+++ app-backend/src/customers/customers.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, Query, UseGuards } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { AuthTokenGuard } from 'src/auth/guard/auth-token.guard';
+import { AdminRolesGuard } from 'src/auth/guard/roles/admin-roles.guard';
 
+@UseGuards(AuthTokenGuard)
 @Controller('clientes')
 export class CustomersController {
   constructor(private readonly customersService: CustomersService) { }
 
+  @UseGuards(AdminRolesGuard)
   @Post('cadastro-cliente')
   create(@Body() createCustomerDto: CreateCustomerDto) {
     return this.customersService.create(createCustomerDto);
@@ -17,11 +21,13 @@ export class CustomersController {
     return this.customersService.findAll();
   }
 
+  @UseGuards(AdminRolesGuard)
   @Patch(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body() updateCustomerDto: UpdateCustomerDto) {
     return this.customersService.update(id, updateCustomerDto);
   }
 
+  @UseGuards(AdminRolesGuard)
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.customersService.remove(id);
