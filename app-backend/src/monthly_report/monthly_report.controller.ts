967aaccd8444710fbbd02e094d953652ad04454f
lucas araujo <lucasao744@gmail.com>
2025-11-06 09:04:15 -0300

    id number

--- app-backend/src/monthly_report/monthly_report.controller.ts
+++ app-backend/src/monthly_report/monthly_report.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { MonthlyReportService } from './monthly_report.service';
 import { CreateMonthlyReportDto } from './dto/create-monthly_report.dto';
 import { UpdateMonthlyReportDto } from './dto/update-monthly_report.dto';
@@ -18,17 +18,17 @@ export class MonthlyReportController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.monthlyReportService.findOne(+id);
+  findOne(@Param('id',ParseIntPipe) id: number) {
+    return this.monthlyReportService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMonthlyReportDto: UpdateMonthlyReportDto) {
-    return this.monthlyReportService.update(+id, updateMonthlyReportDto);
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateMonthlyReportDto: UpdateMonthlyReportDto) {
+    return this.monthlyReportService.update(id, updateMonthlyReportDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.monthlyReportService.remove(+id);
+  remove(@Param('id',ParseIntPipe) id: number) {
+    return this.monthlyReportService.remove(id);
   }
 }
