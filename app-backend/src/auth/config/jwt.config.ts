6199da2b1b7551d6652af8562f6c913bff13cdf9
lucas araujo <lucasao744@gmail.com>
2025-11-12 19:26:11 -0300

    ajuste no auth

--- app-backend/src/auth/config/jwt.config.ts
+++ app-backend/src/auth/config/jwt.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs('jwt', () => {
   return {
-    secret: process.env.JTW_SECRET,
+    secret: process.env.JWT_SECRET,
     audience: process.env.JWT_AUDIENCE,
     issuer: process.env.JWT_TOKEN_ISSUER,
     jwtTtl: process.env.JWT_TTL
